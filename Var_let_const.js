27f013529094058bf28fb003ad86141cd06dfa18
elmaharjan <eli.mhzn@gmail.com>
2020-11-26 10:57:47 +0545

    Diff of Variable, Let and Const

---
+++ Var_let_const.js
@@ -0,0 +1,55 @@
+// Redeclaration allowed
+var x = 56;
+//1000 lines of code
+var x = "sdfs";
+
+//just to run without the result -c Var_let_const.js
+
+//Var is function scoped or global
+//block scoped
+function add()
+{
+    var xx ="jdfkjgj";
+
+    if (xx == "")
+    {
+        let x = 234.6;
+    }
+}
+
+add()
+console.log(xx);
+
+//redeclaration no allowed but can mutate
+let y =45;
+y = "hdghfj";
+
+//no mutation allowed
+// const z = true;
+z= false;
+
+
+
+
+// //Var can be hoisted
+// console.log(age)
+// var age = 67;
+
+
+
+//Var can be hoisted
+console.log(age)
+let age = 67;
+
+
+function add1()
+{
+    var xx ="jdfkjgj";
+
+    if (xx == "")
+    {
+        let x = 234.6;
+    }
+    
+}
+
