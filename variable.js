a5c6ff271173bbee44d62f660618333cb87875b3
elmaharjan <eli.mhzn@gmail.com>
2020-11-26 10:59:55 +0545

    Learning Variables

---
+++ variable.js
@@ -0,0 +1,31 @@
+//primitive
+var a = "Elisha";
+var b = true;
+var c = 345.6;
+var c1 = +Infinity;
+var c2 = -Infinity;
+var c3 = NaN; //not a number
+var x = "dfddfd"/34;
+// console.log(x);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+var d = 3n ** 123n;
+
+var e = undefined;
+var f = null;
+
+//Structural
+ var g = ["a", "b", "c"];
+ var h = {name: "keshab", age: 12};
+
+ console.log(typeof a);
+ console.log(typeof b);
+ console.log(typeof c);
+ console.log(typeof d);
+ console.log(typeof e);
+ console.log(typeof f);
+ console.log(typeof c1);
+ console.log(typeof c2);
+ console.log(typeof c3);
+
+
