e107227bdbfdc0b786e1ab9b75cc41f1d8fcd691
elmaharjan <eli.mhzn@gmail.com>
2020-12-02 13:01:37 +0545

    classwork on regular expression

--- Classwork.js
+++ Classwork.js
@@ -1,7 +1,23 @@
 // Find matching numbers in a paragraph using regular expression
 
 let message = '7H15 M3554G3 53RV35 7O PR0V3 H0W 0UR M1ND5 C4N D0 4M4Z1NG 7H1NG5! 1MPR3551V3 7H1NG5! 1N 7H3 B3G1NN1NG 17 WA5 H4RD BU7 N0W, 0N 7H15 LIN3 Y0UR M1ND 1S R34D1NG 17 4U70M471C4LLY W17H 0U7 3V3N 7H1NK1NG 4B0U7 17, B3 PROUD! 0NLY C3R741N P30PL3 C4N R3AD 7H15.';
-let numbers = /[0-9]/g;
+//let numbers = /[0-9]/g;
+let numbers = /\d+/g;  //+, *, ? are called quantifiers
+//let numbers=/\d*/g;
 let matches = message.match (numbers);
 
 console.log(matches);
+
+
+
+
+//validating email
+
+let mail = 'abc@xyz.com elisha.maharjan@cotiviti.com'
+let regexmail = /[a-z, A-Z,.]+\w*@[a-z]+.(com|net)/g;  //+, *, ? are called quantifiers
+//let numbers=/\d*/g;
+let match = mail.match (regexmail);
+
+console.log(match);
+
+
