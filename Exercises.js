11caed5ebee174383dd157219326722a03d5480e
elmaharjan <eli.mhzn@gmail.com>
2020-12-10 14:02:14 +0545

    Numbers exercise updated

--- Exercises.js
+++ Exercises.js
@@ -38,15 +38,78 @@ function Multitable(m, range = 10) {
 
 // Program to print multiplication from numbers from m to n
 
+
 function Multitable(m, n, range = 10) {
     for (let f = m; f <= n; f++) {
         for (let t = 1; t <= range; t++) {
-            let multi = f * t;
-            console.log(`${f} * ${t} = ${multi}`)
+            let multi = f * t ;
+            console.log(`${f} * ${t} = ${multi}` )
+           
+        }
+           
         }
     }
-}
 
-Multitable(2, 6, 6);
+//Multitable(2, 6, 6);
+
+
+// const people = [
+//     { name: 'Elisha', DOB:'19930409', sex: 'Female'},
+//     { name: 'Ram', DOB:'19950307', sex: 'Male'},
+// ];
+
+// console.table(people);
+
+// process.stdout.write ("Hey");
+// process.stdout.write ("Hey");
+// process.stdout.write ("Hey");
+
+
+
+// Write a string variable with paragraph as value. Find out all the followings:
+    //a. Number of sentences, words and char.
+    //b. Number of special characters.
+    //c. Number of vowels and consonants.
+
+function Para (){
+    for (i=0; i<=j; i++){
+    let paragraph = 'JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else. (Okay, not everything, but it is amazing what you can achieve with a few lines of JavaScript code.)'
+    let j = 
+    console.log(i);
+    console.log(paragraph.split('.'));
+}    
+};
+
+Para();
+
+//Find a random number between provided m and n
+
+
+function getRandom (m, n){
+    return m + Math.random() * (n-m);
+};
+//or
+
+// let getRandom =(m,n) => m + Math.random() * (n-m);
+// console.log(getRandom (2,5));
+
+
+
+// Find a random string of particular length
+
+function getString (length){
+    return Math.random().toString(36).slice(-length);
+};
+
+console.log(getString(7));
+
+
+
+//Find normal distribution of set of Numbers
+
+
+
+
+
 
 
