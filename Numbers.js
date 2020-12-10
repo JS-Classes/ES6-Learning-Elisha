871652e3f57073dcf12d98b6af52b2d47a61a211
elmaharjan <eli.mhzn@gmail.com>
2020-12-10 14:02:43 +0545

    Learning Numbers

---
+++ Numbers.js
@@ -0,0 +1,28 @@
+const a = 5656;
+const b = 0b10101010100010; //binary
+const c = 0o2456; //octal
+const d = 0x12AF; //hex
+
+//Number API
+let age = "67";
+let ageNumber = parseInt(age);
+let ageNumber1 = Number.parseInt(age);
+let ageNumber2 = Number.parseFloat(age);
+
+console.log(typeof NaN);
+
+Number.isNaN();
+Number.isInteger();
+Number.isFinite();
+
+//Math API
+
+Math.abs(-89);
+Math.pow();
+
+let r = Math.random();   // [0,1)
+console.log(r);
+
+
+
+
